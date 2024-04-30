47cbebf690f50b4c6be6c00d7497881a0c554812
Omkar Jadhav <jadhavoj2018@gmail.com>
2024-04-30 19:08:19 +0530

    MultiThreading program added

---
+++ PART-A/MulltiThreading.java
@@ -0,0 +1,28 @@
+
+public class MulltiThreading {
+    public static void main(String[] args) {
+        Mythred thread1 = new Mythred();
+        Mythred thread2 = new Mythred();
+        thread1.start();
+        thread2.start();
+    }
+}
+
+public class Mythred extends Thread
+{
+    public void run()
+    {
+        for(int i = 0 ; i < 5 ; i ++)
+        {
+            System.out.println("Thread " + i);
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
