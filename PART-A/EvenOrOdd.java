8f266b2a866dd0bbf692c8aa3fb39e42bc85d5a5
Omkar Jadhav <jadhavoj2018@gmail.com>
2024-05-08 17:42:50 +0530

    New programs

--- PART-A/EvenOrOdd.java
+++ PART-A/EvenOrOdd.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EvenOrOdd {
+public class evenOrOdd {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
         int x = myScanner.nextInt();
